80a88bb235334c0ea8ba561050509bbc1837ec3a
max-amb <max_a@e.email>
2026-08-03 22:36:09 +0100

    Added recipe

---
+++ recipes/apps/bottles/recipe.nix
@@ -0,0 +1,36 @@
+{
+  pkgs,
+  config,
+  ...
+}:
+
+{
+  apps.bottles = {
+    displayName = "Bottles";
+    description = "Easy-to-use wineprefix manager.";
+    usage = ''
+      Bottles is an application that allows you to easily manage Windows prefixes on your favorite Linux distribution.
+      A Windows prefix is an environment where it is possible to run Windows software using runners, which are compatibility layers capable of running Windows applications on a Linux system.
+      These environments are called bottles.
+
+      For information about the first run see [here](https://usebottles.com/docs/getting-started/first-run), and for more general information see [the bottles website](${config.apps.bottles.links.website}).
+    '';
+
+    icon = ./icon.svg;
+
+    ngi.grants = {
+      Commons = [ "Bottles" ];
+    };
+
+    links = {
+      website = "https://usebottles.com/";
+      docs = "https://usebottles.com/docs";
+      source = "https://github.com/bottlesdevs/Bottles";
+    };
+
+    programs = {
+      mainPackage = pkgs.bottles;
+      runtimes.program.enable = true;
+    };
+  };
+}
