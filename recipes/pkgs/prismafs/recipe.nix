2e9f9c8aec582edb135248bfabaf35b22a90105a
eljamm <fedi.jamoussi@protonmail.ch>
2026-08-26 10:33:51 +0200

    recipes(pkgs.prismafs): use nixos test runner

--- recipes/pkgs/prismafs/recipe.nix
+++ recipes/pkgs/prismafs/recipe.nix
@@ -40,45 +40,31 @@ in
         rm -rf prismafs 
       '';
       installFlags = [ "PREFIX=$(out)" ];
-
-      passthru.tests.nixos = pkgs.testers.runNixOSTest {
-        name = "prismafs-nixos";
-
-        nodes.machine = {
-          environment.systemPackages = [ recipe.result.derivation ];
-          boot.kernelModules = [ "fuse" ];
-        };
-
-        testScript = ''
-          machine.wait_for_unit("multi-user.target")
-
-          machine.succeed("""
-            # see: https://github.com/goranb131/prismaFS/blob/main/test-script.sh
-            BASE=$(mktemp -d)
-            SESSION=$(mktemp -d)
-            MNT=$(mktemp -d)
-            CONF=$(mktemp)
-
-            echo "base $BASE" > $CONF
-            echo "session $SESSION" >> $CONF
-            echo "hello" > $BASE/testfile.txt
-
-            prismafs -c $CONF $MNT
-            sleep 0.5
-
-            ls $MNT | grep testfile.txt
-            cat $MNT/testfile.txt | grep hello
-            cat $MNT/dev/cpu
-
-            umount $MNT
-          """)
-        '';
-      };
-
     };
 
-    test.script = ''
-      prismafs -v | grep -q ${recipe.version}
-    '';
+    test = {
+      script = ''
+        # see: https://github.com/goranb131/prismaFS/blob/main/test-script.sh
+        BASE=$(mktemp -d)
+        SESSION=$(mktemp -d)
+        MNT=$(mktemp -d)
+        CONF=$(mktemp)
+
+        echo "base $BASE" > $CONF
+        echo "session $SESSION" >> $CONF
+        echo "hello" > $BASE/testfile.txt
+
+        prismafs -c $CONF $MNT
+        sleep 0.5
+
+        ls $MNT | grep testfile.txt
+        cat $MNT/testfile.txt | grep hello
+        cat $MNT/dev/cpu
+
+        umount $MNT
+      '';
+      runner = "nixos";
+      nixosConfig.boot.kernelModules = [ "fuse" ];
+    };
   };
 }
